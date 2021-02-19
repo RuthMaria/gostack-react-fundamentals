bc0cf2ef841f0c8815f4595d018062c39f6773b8
RuthMaria <ruth-15rms@hotmail.com>
2021-02-19 12:44:12 -0300

    added background image

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
+
+export const Arroz = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+`;
